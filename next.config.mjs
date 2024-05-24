57d1fb16e1815317e228b0e929cf5091cd0874e3
thecodingrunner <finn.moffett@gmail.com>
2024-05-24 16:08:33 +0100

    retry

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,14 @@
 const nextConfig = {
     images: {
       domains: ['m.media-amazon.com']
-    }
-}
-
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+    },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
 }
 
 export default nextConfig;
