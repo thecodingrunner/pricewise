16b30cd9e23868054f1d0b49216b930746e3b8ac
thecodingrunner <finn.moffett@gmail.com>
2024-05-24 16:06:39 +0100

    ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {
     }
 }
 
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
+
 export default nextConfig;
 
 // /** @type {import('next').NextConfig} */
