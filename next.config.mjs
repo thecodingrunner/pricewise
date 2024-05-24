610380b78c4c298fc85598efb081d0cbd50de869
thecodingrunner <finn.moffett@gmail.com>
2024-05-24 15:45:15 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['m.media-amazon.com']
+    }
+}
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
